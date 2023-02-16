608a0d83e4a0b80a38bb61c94a338fe7294cd4cf
JBegin63 <100534652+JBegin63@users.noreply.github.com>
2023-02-16 15:20:29 -0500

    Finished demo

---
+++ components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import Nav from '../Nav';
+import Footer from '../Footer';
+import styles from './Layout.module.css';
+
+const Layout = ({ children }) => {
+    return (
+        <div className={styles.layout}>
+            <Nav />
+                { children }
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout;
